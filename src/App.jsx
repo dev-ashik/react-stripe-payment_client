b108fb58a8267d9ef6ec947e15aa08b2ded9369f
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2023-04-28 12:54:10 +0800

    payment successful

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import "./App.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [product, setProduct] = useState({
+    name: "Headphone",
+    price: 10,
+  });
+
+  const priceForStripe = product.price * 100;
+
+  const handlePayNow = async (token) => {
+    console.log(token);
+    try {
+      const response = await axios({
+        url: "http://localhost:8000/payment",
+        method: "post",
+        data: {
+          amount: product.price * 100,
+          token,
+        },
+      });
+
+      if (response.status === 200) {
+        toast.success("Payment successful");
+        console.log("Your payment is successful");
+      } else {
+        toast.error("Payment is not successful");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="container">
+      <h2>Complete React & Stripe payment integration</h2>
+      <p>
+        <span>Product: </span>
+        {product.name}
       </p>
-    </>
-  )
+      <p>
+        <span>Price: </span>
+        {product.price}
+      </p>
+
+      <StripeCheckout
+        stripeKey="pk_test_51KOcnlE6mLAE4h3PUxtfXb1ZSl4sQiPAd0AFk0dWetSkd0eSfTfSKHsd8eupNzwhnK4ekgz5SP6xilxSj5de4Zdq00eRzUaBDp"
+        label="Pay Now"
+        name="Pay With Credit Card"
+        billingAddress
+        // shippingAddress
+        amount={{ priceForStripe }}
+        description={`Your total is ${product.price}`}
+        token={handlePayNow}
+      />
+
+      <ToastContainer />
+    </div>
+  );
 }
 
-export default App
+export default App;
